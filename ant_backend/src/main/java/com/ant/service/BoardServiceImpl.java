2bc056e3e2d3db48c4505e65c9a0159581847422
qwe5507 <70142711+qwe5507@users.noreply.github.com>
2021-02-24 22:07:28 +0900

    커뮤니티관련 업뎃

--- ant_backend/src/main/java/com/ant/service/BoardServiceImpl.java
+++ ant_backend/src/main/java/com/ant/service/BoardServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ant.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,6 +24,16 @@ public class BoardServiceImpl implements BoardService {
 		List<BoardVO> boardlist = boardMapper.boardList();
 		return boardlist;
 	}
+	@Override
+	public List<BoardVO> boardListGetLiked() throws Exception {
+		List<BoardVO> boardlist = boardMapper.boardListLiked();
+		return boardlist;
+	}
+	@Override
+	public List<BoardVO> boardListGetSave() throws Exception {
+		List<BoardVO> boardlist = boardMapper.boardListGetSave();
+		return boardlist;
+	}
 
 	@Override
 	public void BoardViewCount(int id) {
@@ -33,6 +45,47 @@ public class BoardServiceImpl implements BoardService {
 		BoardVO board = boardMapper.fetchBoardByID(id);
 		return board;
 	}
+
+	@Override
+	public int updateBoard(BoardVO board) {
+		 
+		return boardMapper.updateBoard(board);
+	}
+
+	@Override
+	public int insertUserLikedBoard(BoardVO board) {
+		System.out.println("insertBoard Service");
+			System.out.println("유저 좋아요 게시판 요청 ");	
+			boardMapper.addUserLikeBoard(board);
+		
+		return 0;
+	}
+
+	@Override
+	public int insertBoard(BoardVO board) {
+		// TODO Auto-generated method stub
+		System.out.println("유저 게시판 추가 요청 / 서비스 ");
+		return 0;
+	}
+
+	@Override
+	public int deleteUserlikeBoard(int board_id, int userid) {
+		Map map = new HashMap();
+		map.put("board_id", board_id);
+		map.put("userid", userid);
+		boardMapper.deleteUserlikeBoard(map);
+		return 0;
+	}
+
+	@Override
+	public int fetchUserlikeBoard(int board_id, int userid) {
+		Map map = new HashMap();
+		map.put("board_id", board_id);
+		map.put("userid", userid);
+		int result = boardMapper.fetchUserlikeBoard(map);
+		return result;
+	}
+
 
 
 
